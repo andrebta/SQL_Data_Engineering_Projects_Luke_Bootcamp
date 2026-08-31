dc7ac17a028b0a1ec0f11ed7f691ebd60eeb273b
Andre Andrade <andrebtaa@gmail.com>
2026-08-31 16:14:43 -0300

    build priority mart and update mart

--- 2_DW_Mart_Build/build_dw_marts.sql
+++ 2_DW_Mart_Build/build_dw_marts.sql
@@ -9,5 +9,11 @@
 -- Step 3: Mart - Create flat mart
 .read 03_create_flat_mart.sql
 
---Step 4: Mart - Create skills demand mart
+-- Step 4: Mart - Create skills demand mart
 .read 04_create_skills_mart.sql
+
+-- Step 5: Mart - Create priority mart
+.read 05_create_priority_mart.sql
+
+-- Step 6: Mart - Update priority mart
+.read 06_update_priority_mart.sql
